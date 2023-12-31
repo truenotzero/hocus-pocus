fe3a930b8673708e9e35ae22302c4c425d7ecd17
truenotzero <truenotzero@gmail.com>
2023-12-31 20:39:38 +0200

    Recursive search now supports any file ending

--- search.c
+++ search.c
@@ -7,7 +7,7 @@ int _hp_expand_path(char const *path, char *buf, int buf_size) {
 
 typedef int(*_hp_on_src_cb_t)(char const *base, char const* src, void *user_data);
 
-int _hp_iterate_dir(char const *base, void *user_data, _hp_on_src_cb_t on_src) {
+int _hp_iterate_dir(char const *base, char const* ending, void *user_data, _hp_on_src_cb_t on_src) {
     char buf[256];
     _hp_expand_path(base, buf, sizeof(buf));
     strncat(buf, "\\*", sizeof(buf));
@@ -22,13 +22,14 @@ int _hp_iterate_dir(char const *base, void *user_data, _hp_on_src_cb_t on_src) {
             int const len = strlen(base) + 1 + strlen(data.cFileName) + 1;
             char *buf = malloc(sizeof(*buf) * len);
             snprintf(buf, len, "%s\\%s", base, data.cFileName);
-            _hp_iterate_dir(buf, user_data, on_src);
+            _hp_iterate_dir(buf, ending, user_data, on_src);
             free(buf);
         }
 
-        // check that the file ends in .c
+        // check that the file ends in the desired ending
         int str_len = strlen(data.cFileName);
-        if (strcmp(&data.cFileName[str_len - 2], ".c") == 0) {
+        int ending_len = strlen(ending);
+        if (strcmp(&data.cFileName[str_len - ending_len], ending) == 0) {
             int on_src_result = on_src(base, data.cFileName, user_data);
             if (on_src_result != 0) return on_src_result;
         }
